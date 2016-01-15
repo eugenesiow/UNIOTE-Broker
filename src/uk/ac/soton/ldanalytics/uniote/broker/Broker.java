f6825a1268a0b4bf869e52b4311c31f70f9e78b4
Eugene Siow <kyo116@gmail.com>
2016-01-15 13:55:27 +0000

    Add automated IP

--- src/uk/ac/soton/ldanalytics/uniote/broker/Broker.java
+++ src/uk/ac/soton/ldanalytics/uniote/broker/Broker.java
@@ -1,5 +1,8 @@
 package uk.ac.soton.ldanalytics.uniote.broker;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Context;
 import org.zeromq.ZMQ.Socket;
@@ -9,13 +12,20 @@ public class Broker {
 		//  Prepare our context and sockets
         Context context = ZMQ.context(1);
 
+        String clientAddress = "";
+        try {
+			clientAddress = InetAddress.getLocalHost().getCanonicalHostName();
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+		}
+        
         //  For publishers
-        Socket frontend =  context.socket(ZMQ.XSUB);
-        frontend.bind("tcp://localhost:5500");
+        Socket frontend = context.socket(ZMQ.XSUB);
+        frontend.bind("tcp://"+clientAddress+":5500");
 
         //  For subscribers
         Socket backend  = context.socket(ZMQ.XPUB);
-        backend.bind("tcp://localhost:5600");
+        backend.bind("tcp://"+clientAddress+":5600");
 
         //  Run the proxy until the user interrupts us
         ZMQ.proxy (frontend, backend, null);
